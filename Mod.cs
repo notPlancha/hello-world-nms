4fa1d4170ebea0327631263d49a654b6cdad4ca3
notPlancha <andre.plancha@hotmail.com>
2022-06-25 13:52:29 +0100

    first commit (not currently working)

---
+++ Mod.cs
@@ -0,0 +1,26 @@
+﻿using Reloaded.Mod.Interfaces;
+using Reloaded.ModHelper;
+using NoMansSky.Api;
+using IReloadedHooks = Reloaded.Hooks.Definitions.IReloadedHooks;
+//used NoMansSky.Api v1.0.1
+#pragma warning disable CA1416
+
+namespace hello_world_nms; 
+
+public class Mod : NMSMod {
+
+    public Mod(IModConfig config, IReloadedHooks hooks, IModLogger logger) : base(config, hooks, logger) {
+        Start();
+    }
+
+    private void Start() {
+        Logger.WriteLine("Hello World!");
+
+
+    }
+
+    public override void Update() {
+        if (Keyboard.IsPressed(Key.Shift)) Logger.WriteLine("Started Running");
+        if (Keyboard.IsReleased(Key.Shift)) Logger.WriteLine("Stopped Running");
+    }
+}
